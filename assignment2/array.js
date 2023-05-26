f903dc4b613f5558220c8afaee14ec30b5ef89bf
shrudeesh j <shrudeeshjithu7123@gamil.com>
2023-05-26 09:18:53 +0000

    assignment

--- assignment2/array.js
+++ assignment2/array.js
@@ -1,7 +1,15 @@
-let num1="11"
-let num2="20"
-let num3="34"
-let num4="50"
-let num5="33"
-let numbar= ["11,20,34,50,33"]
-console.log(number[3]);
+var readlineSync=require("readline-sync");
+let x= readlineSync.question("enter the size of an array: ");
+x=parseInt(x);
+let array=[];
+
+let count=0;
+for(let i=0;i<=(x-1);i++){
+    array[i] = readlineSync.question(`enter the ${i} element: `);
+    array[i]=parseInt(array[i]);
+if(array[i]%2===0){
+    count++;
+}
+}
+console.log("the given array: ",array);
+console.log("number of even numbers in the array: ",count);

8838be84e0dbce338c8ee4554dcbc788bdaded89
shrudeesh j <shrudeeshjithu7123@gamil.com>
2023-06-29 07:16:52 +0000

    object

---
+++ node js/object.js
@@ -0,0 +1,19 @@
+const { log } = require("console");
+const hasOwnProperty = require("has")
+
+let dev={
+Age:23,
+Dob:2000,
+Father:"krishnan",
+mother:"ratha"
+}
+// Object.keys(dev)
+// Object.values(dev)
+// Object.entries(dev)
+// Object.hasOwn(dev,"Age")
+console.log(dev.hasOwnProperty("Father"));
+for (const key in dev) {
+//  console.log(key);
+console.log(key,":",dev[key]);
+
+}
